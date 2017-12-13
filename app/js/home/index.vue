2b0dbc9534aaf54b6d6611224e056e7ff4ea6e9b
yinguohui <yinguohui@bytedance.com>
2017-12-13 13:08:00 +0800

    init project

---
+++ app/js/home/index.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+    <div :class="$style.home">
+        <h1>Home</h1>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" module>
+  @import '../../css/reset.scss';
+  .home{
+    color: red;
+    font-size: 80px;
+  }
+</style>
