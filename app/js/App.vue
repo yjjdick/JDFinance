c93d961b61f45aba922c2ddeceedcb39f14d2be8
yinguohui <yinguohui@bytedance.com>
2017-12-18 00:05:22 +0800

    add layout element

--- app/js/App.vue
+++ app/js/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+    <div id="app">
+        <router-view/>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+    name: "App",
 }
 </script>
