526015310630673a15b912d4cde496833e6f95dc
yinguohui <yinguohui@bytedance.com>
2018-01-06 17:57:47 +0800

    增加压缩、sourceMap

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 module.exports = env => {
   if (!env) {
     env = {}
@@ -20,11 +21,17 @@ module.exports = env => {
           NODE_ENV: '"production"'
         }
       }),
-      new ExtractTextPlugin("style.css", {ignoreOrder: true})
+      new ExtractTextPlugin("style.css", {ignoreOrder: true}),
+      new webpack.optimize.ModuleConcatenationPlugin(),
+      new UglifyJSPlugin({
+        sourceMap: true
+      }),
+
     )
   }
   return {
     entry: ['./app/js/viewport.js','./app/js/main.js'],
+    devtool: env.production?'source-map':'',
     devServer: {
       contentBase: './dist',
       hot: true,
@@ -48,8 +55,8 @@ module.exports = env => {
             },
             extractCSS: true,
             loaders: env.production?{
-              css: ExtractTextPlugin.extract({use: 'css-loader!px2rem-loader?remUnit=40&remPrecision=8', fallback: 'vue-style-loader'}),
-              scss: ExtractTextPlugin.extract({use: 'css-loader!px2rem-loader?remUnit=40&remPrecision=8!sass-loader', fallback: 'vue-style-loader'})
+              css: ExtractTextPlugin.extract({use: 'css-loader?minimize!px2rem-loader?remUnit=40&remPrecision=8', fallback: 'vue-style-loader'}),
+              scss: ExtractTextPlugin.extract({use: 'css-loader?minimize!px2rem-loader?remUnit=40&remPrecision=8!sass-loader', fallback: 'vue-style-loader'})
             }:{
               css: 'vue-style-loader!css-loader!px2rem-loader?remUnit=40&remPrecision=8',
               scss: 'vue-style-loader!css-loader!px2rem-loader?remUnit=40&remPrecision=8!sass-loader'
